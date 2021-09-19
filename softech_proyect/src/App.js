4d8e95fedcd58dcd5cd0313662d25d461a106302
lilianaromerop <lilianaromerop11@gmail.com>
2021-09-19 18:09:34 -0500

    prueba cambios de compatiblidad

--- softech_proyect/src/App.js
+++ softech_proyect/src/App.js
@@ -44,7 +44,7 @@ function App() {
         <section class="contenido">
         <div className="App">
           <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
