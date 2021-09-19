9fd3647578871e1409f809e2c88b20eff959d14f
lilianaromerop <lilianaromerop11@gmail.com>
2021-09-19 17:30:18 -0500

    montaje react del sidebar y searchbar

--- softech_proyect/src/App.js
+++ softech_proyect/src/App.js
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Sporty</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
-    <link  rel="icon"   href="./media/favicon.ico" type="image/png" />
-    <script src="https://kit.fontawesome.com/d4cd33a1a3.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-</head>
-<body>
+import logo from './logo.svg';
+import './App.css';
 
-    <div class="sidebar">
+function App() {
+  return (
+   <div class="template">
+     <div class="sidebar">
         <div class="logo-container">
-            <img class="logo_sporty" src="./media/logoSporty.png" alt="logo Sporty"/>
+            <img class="logo_sporty" src="./logoSporty.png" alt="logo Sporty"/>
         </div>
         <div class="sidebar_options">
             <div class="sidebar_upper_section" >
@@ -30,20 +17,20 @@
                     <li><i class="far fa-list-alt fa-2x sidebar_icon "></i><a class="active" href="#sales">Ventas</a></li>
                 </ul>
             </div>
-            <div class="sidebar_lower_section">   
+            <div class="sidebar_lower_section"> 
                 <ul class="sidebar_lower_text">
                     <li><i class="fas fa-users-cog fa-2x sidebar_icon"></i><a class="active" href="#users">Administrar Usuarios</a></li>
                     <li><i class="fas fa-sign-out-alt fa-2x sidebar_icon"></i><a class="active" href="#new_sale">Cerrar Sesión</a></li>
                 </ul>
             </div>
         </div>
-    </div>
-
+    </div> 
+    
     <main>
-        <header>
+        <header class="navbar">
             <div class="textfield searchText">
                 <i class="fas fa-search"></i>
-                <input type="text" id="searchbar" placeholder="Buscar usuario por ID o nombre" title="Type in a category">
+                <input type="text" id="searchbar" placeholder="Buscar usuario por ID o nombre" title="Type in a category"/>
             </div>
             <div class="user_bar">
                 <div class="infoUser">
@@ -55,12 +42,30 @@
             
         </header>
         <section class="contenido">
+        <div className="App">
+          <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+</div>
 
         </section>
     </main>
+   </div> 
+    
+  );
+}
+
+export default App;
 
 
-    
-    
-</body>
-</html>
