da958a93500793d38c0101c126baf53c3c2e07ca
lilianaromerop <lilianaromerop11@gmail.com>
2021-09-21 22:19:23 -0500

    ajuste tinfo venta y titulo upperbar

--- src/modules/detalleVentas/DetalleVentas.js
+++ src/modules/detalleVentas/DetalleVentas.js
@@ -5,89 +5,77 @@ const DetalleVentas=()=>{
     return(
 
         <>
-          <div class="container-text-over-text">
-                <div class="text-over-text">
-                    <div>
-                        <span class="bolder">Nombre:</span>
+                <div class="container-text-over-text">
+                    <div class="text-over-text">
+                        <div>
+                            <span class="bolder independent-child">ID Responsable:</span>
+                        </div>
+                        <div>
+                            <span>XXX.XXX.XXX - Y</span>
+                        </div>
+                        <div>
+                            <span class="bolder independent-child">Razón Social</span>
+                        </div>
+                        <div>
+                            <span>Juan Camilo Gomez</span>
+                        </div>
+                        <div>
+                            <span class="bolder independent-child">Departamento</span>
+                        </div>
+                        <div>
+                            <span class="">Depto</span>
+                        </div>
                     </div>
-                    <div>
-                        <span>Juan Camilo Gomez</span>
+                    <div class="text-over-text">
+                        <div>
+                            <span class="bolder independent-child">NIT/C.C:</span>
+                        </div>
+                        <div>
+                            <span>D9999999</span>
+                        </div>
+                        <div>
+                            <span class="bolder independent-child">Dirección:</span>
+                        </div>
+                        <div>
+                            <span>10/04/2019</span>
+                        </div>
+                        <div>
+                            <span class="bolder independent-child">Municipio</span>
+                        </div>
+                        <div>
+                            <span>Municipio</span>
+                        </div>
                     </div>
-                </div>
-                <div class="text-over-text">
-                    <div>
-                        <span class="bolder">Fecha de ingreso:</span>
+                    <div class="text-over-text">
+                        <div>
+                            <span class="bolder independent-child">Fecha de pago:</span>
+                        </div>
+                        <div>
+                            <span>11/04/2021</span>
+                        </div>
+                        <div>
+                            <span class="bolder independent-child">Celular:</span>
+                        </div>
+                        <div>
+                            <span>302 1234567</span>
+                        </div>
                     </div>
-                    <div>
-                        <span>10/04/2019</span>
+                    <div class="text-over-text">
+                        <div>
+                            <span class="bolder independent-child">Estado de venta:</span>
+                        </div>
+                        <div>
+                            <span>ruta</span>
+                        </div>
+                        <div>
+                            <span class="bolder independent-child">Email:</span> 
+                        </div>
+                        <div>
+                            <span>ejemplo@ejemplo.com</span>
+                        </div>
                     </div>
-                </div>
-                <div class="text-over-text">
-                    <div>
-                        <span class="bolder">Celular:</span>
-                    </div>
-                    <div>
-                        <span>302 1234567</span>
-                    </div>
-                </div>
-                <div class="text-over-text">
-                    <div>
-                        <span class="bolder">Especialidad:</span> 
-                    </div>
-                    <div>
-                        <span>Seccion Gym</span>
-                    </div>
-                </div>
-            </div>
-            <div class="container-text-over-text">
-                <div class="text-over-text">
-                    <div>
-                        <span class="bolder">Nombre:</span>
-                    </div>
-                    <div>
-                        <span>Juan Camilo Gomez</span>
-                    </div>
-                    <div>
-                        <span class="bolder">Departamento</span>
-                    </div>
-                    <div>
-                        <span class="">Depto</span>
-                    </div>
-                </div>
-                <div class="text-over-text">
-                    <div>
-                        <span class="bolder">Fecha de ingreso:</span>
-                    </div>
-                    <div>
-                        <span>10/04/2019</span>
-                    </div>
-                    <div>
-                        <span class="bolder">Municipio</span>
-                    </div>
-                    <div>
-                        <span>Municipio</span>
-                    </div>
-                </div>
-                <div class="text-over-text">
-                    <div>
-                        <span class="bolder">Celular:</span>
-                    </div>
-                    <div>
-                        <span>302 1234567</span>
-                    </div>
-                </div>
-                <div class="text-over-text">
-                    <div>
-                        <span class="bolder">Especialidad:</span> 
-                    </div>
-                    <div>
-                        <span>Seccion Gym</span>
-                    </div>
-                </div>
-            </div>
-
+                </div>  
         </>
-    
     );
 }
 
--- src/modules/upperBarSection/UpperBarSection.js
+++ src/modules/upperBarSection/UpperBarSection.js
@@ -4,7 +4,7 @@ const UpperBarSection =()=>{
 
     return(
         <div class="barra-ventas">
-            <span class="bolder">Vendedores</span>
+            <span class="bolder title-sales-bar">Vendedores</span>
             <button class="bolder" data-bs-toggle="modal" data-bs-target="#modal-edit-user-info">AGREGAR</button>
         </div >
     )
