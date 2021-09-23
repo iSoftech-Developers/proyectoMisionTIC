6aa25e454174b028915a7e2060e2c61e094cb22f
lilianaromerop <lilianaromerop11@gmail.com>
2021-09-23 18:26:01 -0500

    se termino la seccion con formulario de usuario

--- src/modules/detalleUsuarios/DetalleUsuarios.js
+++ src/modules/detalleUsuarios/DetalleUsuarios.js
@@ -1,10 +1,11 @@
+
 import './DetalleUsuarios.css';
 
 const DetalleUsuarios=()=>{
 
     return(
         <>
-            <div className="modal-body">
+            <div className="max-container">
                 <div className="upper-container-modal">
                     <div className="photo-container">
                         <i className="fas fa-user fa-10x"></i>
@@ -16,11 +17,11 @@ const DetalleUsuarios=()=>{
                             </div>
                             <div>
                                 <span className="user-id">023466</span>
-                            </div>
+                            </div>  
                         </div>
 
                         <div className="lower-info-container-modal">
-                            <div className="form-lower-info-container">
+                            <div className="lower-info-container">
                                 <div className="text-over-text">
                                     <div>
                                         <span className="bolder">Nombre</span>
@@ -53,35 +54,20 @@ const DetalleUsuarios=()=>{
                                         <span>Sección Gym</span>
                                     </div>
                                 </div>
-                                </div>
                             </div>
                         </div>
+                    </div>
                 </div>
-
                 <div className="lower-container-modal-form">
                     <div className="text-over-text">
                         <div>
-                            <span className="bolder">ID*</span>
+                            <span className="bolder gray-letter">ID*</span>
                         </div>
                         <div>
                             <input className="input-form-users" type="text" ></input>
                         </div>
                         <div>
-                            <span className="bolder independent-child">Número Celular</span>
-                        </div>
-                        <div>
-                            <input className="input-form-users" type="text" ></input>
-                        </div>
-                    </div>
-                    <div className="text-over-text">
-                        <div>
-                            <span className="bolder ">Nombres y Apellidos*</span>
-                        </div>
-                        <div>
-                            <input className="input-form-users" type="text" ></input>
-                        </div>
-                        <div>
-                            <span className="bolder independent-child">Especialidad*</span>
+                            <span className="bolder gray-letter independent-child">Número Celular</span>
                         </div>
                         <div>
                             <input className="input-form-users" type="text" ></input>
@@ -89,24 +75,40 @@ const DetalleUsuarios=()=>{
                     </div>
                     <div className="text-over-text">
                         <div>
-                            <span className="bolder">Fecha de ingreso*</span>
+                            <span className="bolder gray-letter">Nombres y Apellidos*</span>
                         </div>
                         <div>
                             <input className="input-form-users" type="text" ></input>
                         </div>
                         <div>
-                            <span className="bolder independent-child">Rol*</span>
+                            <span className="bolder gray-letter independent-child">Especialidad*</span>
                         </div>
                         <div>
                             <input className="input-form-users" type="text" ></input>
                         </div>
                     </div>
-                </div> 
-
+                    <div className="text-over-text">
+                        <div>
+                            <span className="bolder gray-letter">Fecha de ingreso*</span>
+                        </div>
+                        <div>
+                            <input className="input-form-users" type="text" ></input>
+                        </div>
+                        <div>
+                            <span className="bolder gray-letter independent-child">Rol*</span>
+                        </div>
+                        <div>
+                            <input className="input-form-users" type="text" ></input>
+                        </div>
+                    </div>
+                </div>
+                
                 <div className="modal-bottom">
                     <button className="bolder save-button"  data-bs-toggle="modal" data-bs-target="#modal-edit-user-info">GUARDAR</button >
-                </div>
+                </div>    
             </div>
+
+            
         </>
     );
 }
