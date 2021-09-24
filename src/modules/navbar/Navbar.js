efce82957233743acdbc13d4b902eaf26bd66264
iSebastianCabrera <jscrojas25@yahoo.es>
2021-09-23 21:03:52 -0500

    cambios en el ancho maximo

--- src/modules/navbar/Navbar.js
+++ src/modules/navbar/Navbar.js
@@ -4,16 +4,18 @@ const Navbar=()=>{
 
   return (
     <header>
-    <div className="textfield searchText">
-        <i className="fas fa-search"></i>
-        <input type="text" id="searchbar" placeholder="Buscar usuario por ID o nombre" title="Type in a category"/>
-    </div>
-    <div className="user_bar">
-        <div className="text-over-text infoUser">
-            <span className="bolder">ID 2345</span>
-            <span clase="nameUser">Juan Pérez Pérez</span>
+      <div class ="navbar-container">
+        <div className="textfield searchText">
+            <i className="fas fa-search"></i>
+            <input type="text" id="searchbar" placeholder="Buscar usuario por ID o nombre" title="Type in a category"/>
         </div>
-        <i className="fas fa-user-circle fa-3x"></i>
+        <div className="user_bar">
+            <div className="text-over-text infoUser">
+                <span className="bolder">ID 2345</span>
+                <span clase="nameUser">Juan Pérez Pérez</span>
+            </div>
+            <i className="fas fa-user-circle fa-3x"></i>
+        </div>
     </div>
     
 </header>
--- src/modules/sidebar/Sidebar.js
+++ src/modules/sidebar/Sidebar.js
@@ -13,16 +13,16 @@ const Siderbar =()=>{
         <div className="sidebar_options">
             <div className="sidebar_upper_section" >
                 <ul className="sidebar_upper_text">
-                    <li className="sidebar-li"><i className="fas fa-plus-circle fa-2x sidebar_icon"></i><Link className="active" to="/nuevaVenta">Nueva venta</Link></li>
-                    <li className="sidebar-li"><i className="fas fa-users fa-2x sidebar_icon "></i><Link className="active" to="/moduloClientes">Clientes</Link></li>
-                    <li className="sidebar-li"><i className="fas fa-user-tag fa-2x sidebar_icon"></i><Link className="active" to="/moduloVendedores">Vendedores</Link></li>
-                    <li className="sidebar-li"><i className="far fa-list-alt fa-2x sidebar_icon "></i><Link className="active" to="/moduloVentas">Ventas</Link></li>
+                    <Link className="active sidebar-li sidebar-link" to="/nuevaVenta"><i className="fas fa-plus-circle fa-2x sidebar_icon"></i>Nueva venta</Link>  
+                    <Link className="active sidebar-li sidebar-link" to="/moduloClientes"><i className="fas fa-users fa-2x sidebar_icon"></i>Clientes</Link> 
+                    <Link className="active sidebar-li sidebar-link" to="/moduloVendedores"><i className="fas fa-user-tag fa-2x sidebar_icon"></i>Vendedores</Link>
+                    <Link className="active sidebar-li sidebar-link" to="/moduloVentas"><i className="far fa-list-alt fa-2x sidebar_icon"></i>Ventas</Link> 
                 </ul>
             </div>
             <div className="sidebar_lower_section">   
                 <ul className="sidebar_lower_text">
-                    <li className="sidebar-li" ><i className="fas fa-users-cog fa-2x sidebar_icon"></i><Link className="active" to="#users">Administrar Usuarios</Link></li>
-                    <li className="sidebar-li"><i className="fas fa-sign-out-alt fa-2x sidebar_icon"></i><Link className="active" to="#new_sale">Cerrar Sesión</Link></li>
+                    <Link className="active sidebar-li sidebar-link" to="/moduloVentas"><i className="fas fa-users-cog fa-2x sidebar_icon"></i>Administrar Usuarios</Link>
+                    <Link className="active sidebar-li sidebar-link" to="/"><i className="fas fa-sign-out-alt fa-2x sidebar_icon"></i>Cerrar Sesión</Link>
                 </ul>
             </div>
         </div>
