b01e3f76a64a5550b5546c9f328dd1c8236cbd48
iSebastianCabrera <jscrojas25@yahoo.es>
2021-09-26 15:42:25 -0500

    cambios tablas

--- src/modules/cards/Cards.js
+++ src/modules/cards/Cards.js
@@ -10,12 +10,11 @@ const Cards=(props)=>{
                     <div className="flex">
                         <i className="fas fa-user text-white bg-gray-800 mr-4 pt-4 px-3 pb-2"></i>
                         <div className="card-info w-full align-center flex justify-between">
-                            <span className="bolder">{props.titleCard}</span>
-                            <button className="edit-card"><i className="fas fa-pen"></i></button>
+                            <span className="font-semibold">{props.titleCard}</span>
+                            <button className="edit-card bg-gray-50 border-0"><i className="fas fa-pen text-gray-400"></i></button>
                         </div>
                     </div>
-                    <div className="card-detail">
-                        <table>
+                        <table className="text-sm w-full bg-white border-solid mt-4">
                             <tr>
                                 <th >{props.field1}</th>
                                 <th >{props.field2}</th>
@@ -32,8 +31,6 @@ const Cards=(props)=>{
                                 <td align="center">11-04-2021</td>
                             </tr>
                         </table>
-
-                    </div>
                 </div>
             </div>
 
