3c828f4b4baa9c1879d03363f8dd6f6c9fe00d36
lilianaromerop <lilianaromerop11@gmail.com>
2021-09-26 20:48:51 -0500

    ajuste titulos secciones tailwind

--- src/modules/navbar/Navbar.js
+++ src/modules/navbar/Navbar.js
@@ -3,22 +3,21 @@ import './Navbar.css';
 const Navbar=()=>{
 
   return (
-    <header>
-    
-      <div className ="navbar-container">
-        <div className="textfield searchText">
-            <i className="fas fa-search"></i>
-            <input type="text" id="searchbar" placeholder="Buscar usuario por ID o nombre" title="Type in a category"/>
-        </div>
-        <div className="user_bar">
-            <div className="text-over-text infoUser">
-                <span className="bolder">ID 2345</span>
-                <span clase="nameUser">Juan Pérez Pérez</span>
-            </div>
-            <i className="fas fa-user-circle fa-3x"></i>
-        </div>
-    </div>
-    </header>
+      <header>
+        <div className ="navbar-container">
+          <div className="textfield w-1/3 h-8">
+              <i className="fas fa-search"></i>
+              <input type="text" placeholder="Buscar usuario por ID o nombre" title="Type in a category"/>
+          </div>
+          <div className="user_bar">
+              <div className="text-over-text infoUser">
+                  <span className="bolder">ID 2345</span>
+                  <span clase="nameUser">Juan Pérez Pérez</span>
+              </div>
+              <i className="fas fa-user-circle fa-3x"></i>
+          </div>
+      </div>
+      </header>
 
   )
 }
