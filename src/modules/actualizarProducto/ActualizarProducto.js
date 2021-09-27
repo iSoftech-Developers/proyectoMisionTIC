06dc835bfc755cfad472a1061229e0b4f185031d
lilianaromerop <lilianaromerop11@gmail.com>
2021-09-27 17:21:20 -0500

    ajuste actualizar producto boton submit

--- src/modules/actualizarProducto/ActualizarProducto.js
+++ src/modules/actualizarProducto/ActualizarProducto.js
@@ -4,7 +4,7 @@ const ActualizarProducto = (props) => {
     
     return (
         <form className="space-y-8">
-                <div className="form-upper-section flex justify-between font-bold label-color">
+                <div className="form-upper-section flex justify-between font-bold label-color space-x-4">
                     <div className="w-1/5">
                         <label for="productid">{props.formLabelTitle.label1}</label>
                         <input required class=" w-full h-8 p-2" type="text" name="productid" id="productid"/>
@@ -21,17 +21,18 @@ const ActualizarProducto = (props) => {
                         <label for="productvalue">{props.formLabelTitle.label4}</label>
                         <input required class=" w-full h-8 p-2" type="text" name="productvalue" id="productvalue"/>
                     </div> 
-                </div>
-                <div className="form-lower-section flex justify-between font-bold label-color">
                     <div className="w-1/5">
-                        <label for="productstatus">{props.formLabelTitle.label6}</label>
+                        <label for="productstatus">{props.formLabelTitle.label5}</label>
                         <select required class=" w-full h-8 font-bold" name="productstatus">
-                            <option value="status">Estado</option>
                             <option value="available">Disponible</option>
                             <option value="notavailable">No disponible</option>
                         </select>
                     </div>
                 </div>
+                <div className=" w-full flex justify-center">
+                    <input className="w-1/5 cursor-pointer bg-green-400 h-10 text-white font-bold m-10" type="submit" value="Guardar"/>
+                </div>
+               
             </form>
     )
 }
