c64b5a68a7a2e1d4174a7728a484aae030049d41
Raul Acevedo <acevedog.raul@gmail.com>
2021-09-27 13:32:50 -0500

    nuevos cambios

--- src/pages/PaginaProductos.js
+++ src/pages/PaginaProductos.js
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import Sidebar from "../modules/sidebar/Sidebar";
-import Navbar from "../modules/navbar/Navbar";
-import NoButtonUpperBarSection from "../modules/noButtonUpperBarSection/NoButtonUpperBarSection";
-import Tablas from "../modules/tablas/tablas";
-
-
-const PaginaProductos =()=>{
-
-
-    return(
-      <>
-
-      <Sidebar />
-      <main>
-        <Navbar/>
-      
-          <section className="contenido">
-          <NoButtonUpperBarSection titlePageProduct="Listado de Productos"/>
-          <Tablas titleListProduct="" field1="ID Producto" field2="Producto" field3="Precio" field4="Talla" field5="Cantidad" field6="Total"/>
-           
-          </section>
-      </main>
-      </>
-
-    );
-
-}
-
-
-export default PaginaProductos;
-=======
 import Sidebar from '../modules/sidebar/Sidebar';
 import Navbar from '../modules/navbar/Navbar';
 import Cards from '../modules/cards/Cards';
@@ -57,4 +25,3 @@ const PaginaProductos =()=> {
     );
   }
   export default PaginaProductos;
->>>>>>> edbb6b30ec31bc4ee6715a183efc10dd34f32c2b
