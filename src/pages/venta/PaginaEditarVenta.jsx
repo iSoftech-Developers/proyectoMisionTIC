c4462b72c5afb34036cd324f3a3fc776337411de
Raul Acevedo <acevedog.raul@gmail.com>
2021-09-28 19:04:55 -0500

    ajustes importaciones no necesarias

--- src/pages/venta/PaginaEditarVenta.jsx
+++ src/pages/venta/PaginaEditarVenta.jsx
@@ -1,8 +1,6 @@
 import Sidebar from '../../modules/sidebar/Sidebar';
 import Navbar from '../../modules/navbar/Navbar';
-import DetalleVentas from '../../modules/detalleVentas/DetalleVentas';
 import NoButtonUpperBarSection from '../../modules/noButtonUpperBarSection/NoButtonUpperBarSection';
-import NuevaVenta from '../../modules/nuevaVenta/NuevaVenta';
 import EditarVenta from '../../modules/editarVenta/EditarVenta';
 
 
@@ -19,6 +17,7 @@ const PaginaEditarVenta =()=>{
                     <EditarVenta formlabel1="ID Venta" formlabel2="Fecha Incial de Pago*" formlabel3="Producto*" formlabel4="Fecha Final de Pago*" formlabel5="Cantidad*" formlabel6="Estado de la Venta*" formlabel7="Valor Total Venta*" formlabel8="Responsable" nameButton="GUARDAR" selectorButton="/modules/ok/Ok"/>
                     
                 </section>
+
             </section>
         </main>
        </> 
