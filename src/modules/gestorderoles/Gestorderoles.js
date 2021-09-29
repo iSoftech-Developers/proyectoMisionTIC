3309ed933fe72d3253b6cf13dbee998a9764e77e
floza97 <florezdiego007@gmail.comm>
2021-09-28 22:25:49 -0500

    avance de carts

--- src/modules/gestorderoles/Gestorderoles.js
+++ src/modules/gestorderoles/Gestorderoles.js
@@ -1,7 +1,7 @@
 import img_avatar from '../../media/img_avatar.png';
 import { Link } from 'react-router-dom';
 
-const Gestorderoles = (props) => {
+const Gestorderoles = () => {
   return (
     <>
       <div className="w-1/4">
@@ -12,22 +12,42 @@ const Gestorderoles = (props) => {
         </select>
       </div>
       <div className=" flex justify-between mt-32 w-80">
-        <div className="card m-3 w-60 ml-3  bg-white rounded-lg">
+        <div className="card m-3 w-60 ml-3  bg-white rounded-lg cursor-pointer transition duration-250 ease-in-out transform hover:-translate-y-1 hover:scale-110 shadow-md  ">
             <div className="content_imageuser flex w-20rem ">
                 <img src={img_avatar} alt="Avatar" className="m-0.1 object-fill border rounded-md m-2 w-25 h-15" />
             </div>
-            <div class="container">
-                <div className="container_Idusername ml-3">
+            <div className="container " >
+                <div className="container_Idusername ml-3 font-black">
                     <h4>ID 0023455968</h4>
                 </div>
-                <div className="container_username ml-3">
-                    <p>Architect & Engineer</p>
+                <div className="container_username ml-3  ">
+                    <p>Architect & Engineer </p>
                 </div>
-                <div className="container_rol ml-3">
-                    <p className="text_rol text-white bg-black rounded-lg w-25">Administrador</p>
+                <div className="container_rol ml-3 bg-black rounded-lg m-3 mr-21 ">
+                    <p className="text_rol text-white ml-2 " >Administrador</p>
                 </div>
             </div>
         </div>
+        <div className="card m-3 w-60 ml-3  bg-white rounded-lg cursor-pointer transition duration-250 ease-in-out transform hover:-translate-y-1 hover:scale-110 shadow-lg">
+            <div className="content_imageuser flex w-20rem ">
+                <img src={img_avatar} alt="Avatar" className="m-0.1 object-fill border rounded-md m-2 w-25 h-15" />
+            </div>
+            <div className="container static">
+                <div className="container_Idusername ml-3 font-black">
+                    <h4>ID 0023455968</h4>
+                </div>
+                <div className="container_username ml-3 ">
+                    <p>Architect & Engineer</p>
+                </div>
+                <div className="container_rol ml-3 bg-black rounded-lg m-3 mr-21">
+                    <p className="text_rol text-white ml-2 " >Administrador</p>
+                </div>
+            </div>
+        </div>
+       
+        
+        
+        
       
     </div>
       
