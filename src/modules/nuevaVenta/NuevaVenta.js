dbc16fcd154e8d0ab7c6f109e930d0c64a62df66
iSebastianCabrera <jscrojas25@yahoo.es>
2021-10-13 16:18:29 -0500

    reparando la venta

--- src/modules/nuevaVenta/NuevaVenta.js
+++ src/modules/nuevaVenta/NuevaVenta.js
@@ -9,8 +9,10 @@ const NuevaVenta=()=>{
     const form = useRef(null);
     const [vendedores, setVendedores] = useState([]);
     const [productos, setProductos] = useState([]);
+    const [clientes, setClientes] = useState([]);
     const [productosTabla, setProductosTabla] = useState([]);
     const [ejecutarConsulta, setEjecutarConsulta] = useState(true);
+
 
     useEffect(() => {
         const fetchVendores =  () => {
@@ -19,14 +21,16 @@ const NuevaVenta=()=>{
         const fetchProductos=()=>{
             obtenerDB(setProductos,setEjecutarConsulta,"http://localhost:3001/productos")
         }
+        const fetchClientes=()=>{
+          obtenerDB(setClientes,setEjecutarConsulta,"http://localhost:3001/clientes")
+      }
 
         fetchVendores();
         fetchProductos();
+        fetchClientes();
+        console.log(vendedores)
     },[]);
    
-
-
-
 
     const submitForm = (e) => {
         e.preventDefault();
@@ -62,10 +66,10 @@ const NuevaVenta=()=>{
 
       const datosVenta = {
         vendedor: vendedores.filter((v) => v._id === nuevaVenta.vendedor)[0],
-        vehiculos: listaProductos,
+        productos: listaProductos,
       };
   
-      console.log('lista vehiculos', datosVenta);
+      console.log('lista productos', datosVenta);
 
     
     toast.success('Venta guardada');
@@ -81,7 +85,10 @@ const NuevaVenta=()=>{
                             <select name='vendedor' className='p-2' defaultValue='' required >
                                 <option disabled value=''> Seleccione un Vendedor </option>
                                 {vendedores.map((el) => {
-                                return <option key={nanoid()} value={el._id}>{`${el.field1}`}</option>;
+                                  if (el.field4==="Vendedor"){
+                                    return (<option key={nanoid()} value={el._id}>{`${el.field1}`}</option>);
+                                  }
+                                  return null;
                                 })}
                             </select>
                         </label>                    
@@ -117,20 +124,19 @@ const NuevaVenta=()=>{
                             </select>
                         </div> 
                     <div className="w-1/6">
-                        <input required className="cursor-pointer w-full mt-4 h-10 rounded text-white bg-gray-400" type="button" name="userspecial" id="userspecial" value="Productos"/>
                     </div>
+                </div>
+                <div className= "w-full">
+                  <TablaProductos
+                  productos={productos}
+                  setProductos={setProductos}
+                  setProductosTabla={setProductosTabla}/>
                 </div>
                 <div className=" w-full flex justify-center">
                     <input className="w-1/6 cursor-pointer bg-green-400 h-10 rounded text-white font-bold my-16" type="submit" value="Guardar"/>
                     <ToastContainer position="top-right" autoClose={5000}/>
                 </div>
-                <div className= "w-full">
 
-                <TablaProductos
-                productos={productos}
-                setProductos={setProductos}
-                setProductosTabla={setProductosTabla}/>
-                </div>
             </form>
            
     </div>
@@ -140,20 +146,28 @@ const NuevaVenta=()=>{
 const TablaProductos = ({ productos, setProductos, setProductosTabla }) => {
     const [productoAAgregar, setProductoAAgregar] = useState({});
     const [filasTabla, setFilasTabla] = useState([]);
+    const [buttonActive, setButtonActive] = useState('hidden');
   
     useEffect(() => {
-      console.log(productoAAgregar);
+      if(Object.keys(productoAAgregar).length !== 0){
+        console.log(productoAAgregar);
+        setButtonActive("")  
+      }
+     
     }, [productoAAgregar]);
   
     useEffect(() => {
       console.log('filasTabla', filasTabla);
       setProductosTabla(filasTabla);
     }, [filasTabla, setProductosTabla]);
+
+   
   
     const agregarNuevoProducto = () => {
       setFilasTabla([...filasTabla, productoAAgregar]);
       setProductos(productos.filter((v) => v._id !== productoAAgregar._id));
       setProductoAAgregar({});
+      setButtonActive("hidden")  
     };
   
     const eliminarProducto = (prodcutoAEliminar) => {
@@ -168,11 +182,15 @@ const TablaProductos = ({ productos, setProductos, setProductosTabla }) => {
             <select
               className='p-2'
               value={productoAAgregar._id ?? ''}
-              onChange={(e) =>
+              onChange={(e) =>{
                 setProductoAAgregar(productos.filter((v) => v._id === e.target.value)[0])
+                if(e.target.value==="Seleccione un Producto"){
+                  
+                }
+               }
               }
             >
-              <option disabled value=''>
+              <option name="opcion" disabled value=''>
                 Seleccione un Producto
               </option>
               {productos.map((el) => {
@@ -188,9 +206,8 @@ const TablaProductos = ({ productos, setProductos, setProductosTabla }) => {
           <button
             type='button'
             onClick={() => agregarNuevoProducto()}
-            className='col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white'
-          >
-            Agregar Vehículo
+            className= {`col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white ${buttonActive}`}>
+            Agregar Producto
           </button>
         </div>
         <table className='table-fixed w-full bg-white border-solid border-gray-300 border my-5'>
