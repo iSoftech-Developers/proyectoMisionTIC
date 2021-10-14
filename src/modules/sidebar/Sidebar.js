8d9e50eb6a7627a23ccde0aee0e486e2b21760c2
lilianaromerop <lilianaromerop11@gmail.com>
2021-10-14 18:10:01 -0500

    ajuste boton cerrar sesion

--- src/modules/sidebar/Sidebar.js
+++ src/modules/sidebar/Sidebar.js
@@ -18,20 +18,19 @@ const Siderbar =()=>{
         <div className="h-full mt-24 text-base justify-between flex flex-col">
             <div>
                 <ul className="flex flex-col">
-                    <Ruta  ruta="/dashboard" icono="fas fa-home far"title="Dashboard" /> 
-                    <Ruta  ruta="/nuevaVenta" icono="fas fa-plus-circle "title="Nueva venta"/>  
-                    <Ruta  ruta="/moduloClientes" icono="fas fa-users"title="Clientes" /> 
-                    <Ruta  ruta="/moduloVendedores" icono="fas fa-user-tag  "title="Vendedores"/>
-                    <Ruta  ruta="/moduloVentas" icono="far fa-list-alt"title="Ventas"/>
-                    <Ruta  ruta="/productos" icono="fas fa-tshirt far"title="Productos"/> 
+                    <Ruta ruta="/dashboard" icono="fas fa-home far"title="Dashboard" /> 
+                    <Ruta ruta="/nuevaVenta" icono="fas fa-plus-circle "title="Nueva venta"/>  
+                    <Ruta ruta="/moduloClientes" icono="fas fa-users"title="Clientes" /> 
+                    <Ruta ruta="/moduloVendedores" icono="fas fa-user-tag "title="Vendedores"/>
+                    <Ruta ruta="/moduloVentas" icono="far fa-list-alt"title="Ventas"/>
+                    <Ruta ruta="/productos" icono="fas fa-tshirt far"title="Productos"/> 
                 </ul>
             </div>
-            <div className=" mb-10">   
-                <ul className="flex flex-col">
+            <div className=" mb-10">
+                <ul className="flex flex-col ">
                     <Ruta ruta="/rolesUsuarios"icono="fas fa-users-cog"title="Administrar Usuarios"/>
-                    <button className="hover:bg-gray-500   w-full py-2 text-gray-300" onClick={() => logout({ returnTo: window.location.origin })}  >
-                    <i className=" fas fa-sign-out-alt fa-lg text-gray-300  w-9 "></i>Cerrar Sesión </button>
-
+                        <button className=" text-left hover:bg-gray-500 py-2 text-gray-300 pl-5" onClick={() => logout({ returnTo: window.location.origin })}  >
+                        <i className="fas fa-sign-out-alt fa-lg text-gray-300 w-9 "></i>Cerrar Sesión </button>
                     
                     {/*<Ruta ruta="/login" icono="fas fa-sign-out-alt"title="Cerrar Sesión"/>*/}
                     
@@ -41,20 +40,20 @@ const Siderbar =()=>{
         </div>
     </div>
     )
-  }
+}
 
 
- const Ruta = ({ruta,icono,title})=>{
-  const isActive = UseActiveRoute(ruta);
-  return(
+const Ruta = ({ruta,icono,title})=>{
+const isActive = UseActiveRoute(ruta);
+return(
     <>
-     <Link to={ruta} className={`hover:bg-gray-500 w-full py-2 text-gray-${isActive?'800':'300'} bg-gray-${isActive?'600':'900'}`}><i className={`${icono} fa-lg text-${isActive?'900':'300'} ml-5 w-9`}></i>{title}
-     </Link>
-     </>
+    <Link to={ruta} className={`hover:bg-gray-500 w-full py-2 text-gray-${isActive?'800':'300'} bg-gray-${isActive?'600':'900'}`}><i className={`${icono} fa-lg text-${isActive?'900':'300'} ml-5 w-9`}></i>{title}
+    </Link>
+    </>
     );
-  }
-
- 
+}
 
 
-  export default Siderbar;
+
+
+export default Siderbar;
