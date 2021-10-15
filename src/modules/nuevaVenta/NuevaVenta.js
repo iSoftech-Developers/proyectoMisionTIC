a2c805a297bb485631730596662948101a89e914
iSebastianCabrera <jscrojas25@yahoo.es>
2021-10-15 15:53:02 -0500

    cambios en el agregar venta

--- src/modules/nuevaVenta/NuevaVenta.js
+++ src/modules/nuevaVenta/NuevaVenta.js
@@ -15,133 +15,12 @@ const NuevaVenta=()=>{
     const [CapturaInput,setCapturaInput]=useState();
     const [clienteSelect,setClienteSelect]=useState({})
     const [capturaNumber,setCapturaNumber]=useState();
-    const [productoAAgregar, setProductoAAgregar] = useState({});
-    const [filasTabla, setFilasTabla] = useState([]);
-    const [buttonActive, setButtonActive] = useState('hidden');
 
 
 
 
-    const TablaProductos = ({ productos, setProductos, setProductosTabla,capturaNumber,setCapturaNumber }) => {
-  
 
-
-      useEffect(() => {
-        if(Object.keys(productoAAgregar).length !== 0){
-          console.log(productoAAgregar);
-          setButtonActive("")  
-        }
-       
-      }, [productoAAgregar]);
-    
-      useEffect(() => {
-        console.log('filasTabla', filasTabla);
-        setProductosTabla(filasTabla);
-      }, [ setProductosTabla]);
-    
-    
-      useEffect(() => {
-        console.log(capturaNumber);
-        
-      },);
-    
-    
-     
-    
-      const agregarNuevoProducto = () => {
-        setFilasTabla([...filasTabla, productoAAgregar]);
-        setProductos(productos.filter((v) => v._id !== productoAAgregar._id));
-        setProductoAAgregar({});
-        setButtonActive("hidden")  
-      };
-    
-      const eliminarProducto = (prodcutoAEliminar) => {
-        setFilasTabla(filasTabla.filter((v) => v._id !== prodcutoAEliminar._id));
-        setProductos([...productos, prodcutoAEliminar]);
-      };
-    
-      return (
-        <>
-          <div className='flex w-full justify-between'>
-            <label className='flex flex-col' htmlFor='produto'>
-              <select
-                className='p-2'
-                value={productoAAgregar._id ?? ''}
-                onChange={(e) =>{
-                  setProductoAAgregar(productos.filter((v) => v._id === e.target.value)[0])
-                  if(e.target.value==="Seleccione un Producto"){
-                    
-                  }
-                 }
-                }
-              >
-                <option name="Seleccione un Producto" disabled value=''>
-                  Seleccione un Producto
-                </option>
-                {productos.map((el) => {
-                  return (
-                    <option
-                      key={nanoid()}
-                      value={el._id}
-                    >{`${el.field1} ${el.field6} ${el.field4} ${el.field7}`}</option>
-                  );
-                })}
-              </select>
-            </label>
-            <button
-              type='button'
-              onClick={() => agregarNuevoProducto()}
-              className= {`col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white ${buttonActive}`}>
-              Agregar Producto
-            </button>
-          </div>
-          <table className='table-fixed w-full bg-white border-solid border-gray-300 border my-5'>
-            <thead>
-              <tr>
-                <th>Id</th>
-                <th>Nombre</th>
-                <th>Genero</th>
-                <th>Talla</th>
-                <th>Color</th>
-                <th>Cantidad</th>
-                <th>Precio Unitario</th>
-                <th>Valor total</th>
-                <th>Eliminar</th>
-                <th className='hidden'>Input</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filasTabla.map((el, index) => {
-                return (
-                  <tr key={nanoid()}>
-                    <td align="center" >{el.ids}</td>
-                    <td align="center">{el.field1}</td>
-                    <td align="center">{el.field6}</td>
-                    <td align="center">{el.field4}</td>
-                    <td align="center">{el.field7}</td>
-                    <td align="center">
-                      <label htmlFor={`cantidad_${index}`}>
-                        <input  type='text' name={`cantidad_${index}`}  id={`cantidad_${index}`}   />
-                      </label>  
-                    </td>
-                    <td align="center">{el.field2}</td>
-                    <td align="center">{el.field2}</td>
-                    <td align="center">
-                      <i
-                        onClick={() => eliminarProducto(el)}
-                        className='fas fa-minus text-red-500 cursor-pointer'
-                      />
-                    </td>
-                    <input hidden defaultValue={el._id} name={`producto_${index}`} />
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </>
-      );
-    };
-    
+   
   
   
 
@@ -202,10 +81,11 @@ const NuevaVenta=()=>{
   
       console.log('lista despues de cantidad', listaProductos);
 
-
       const datosVenta = {
         vendedor: vendedores.filter((v) => v._id === nuevaVenta.vendedor)[0],
         productos: listaProductos,
+        cliente:clientes.filter((i)=>i._id===clienteSelect._id)[0],
+        
       };
   
       console.log('lista productos', datosVenta);
@@ -218,11 +98,7 @@ const NuevaVenta=()=>{
     useEffect(()=>{
       clientes.map((cliente)=>{
         if(cliente.field2===CapturaInput){
-          setClienteSelect({
-             nombre:cliente.field1,
-             telefono:cliente.field5,
-             ciudad:cliente.field4,
-             email:cliente.field3})
+          setClienteSelect(cliente)
         }
         return null;
       })
@@ -259,7 +135,7 @@ const NuevaVenta=()=>{
                     </div>
                     <div className="w-1/6">
                         <label htmlFor="clienteName">Nombre cliente</label>
-                        <input required  className=" w-full h-8 p-2 rounded-md input-border" type="text" name="clienteName" id="clienteName" value={clienteSelect.nombre}/>
+                        <input required  className=" w-full h-8 p-2 rounded-md input-border" type="text" name="clienteName" id="clienteName" value={clienteSelect.field1}/>
                     </div>
                     <div className="w-1/6">
                         <label htmlFor="payDate">Fecha de pago</label>
@@ -269,15 +145,15 @@ const NuevaVenta=()=>{
                 <div className="form-lower-section flex justify-between font-bold label-color">
                     <div className="w-1/6 ">
                             <label htmlFor="celNumber">Teléfono</label>
-                            <input required className=" w-full h-8 p-2 input-border" type="text" name="celNumber" id="celNumber" value={clienteSelect.telefono}/>
+                            <input required className=" w-full h-8 p-2 input-border" type="text" name="celNumber" id="celNumber" value={clienteSelect.field5}/>
                         </div>
                         <div className="w-1/6">
                             <label htmlFor="clienteCity">Ciudad</label>
-                            <input required className=" w-full h-8 p-2 input-border" type="text" name="clienteCity" id="clienteCity" value={clienteSelect.ciudad}/>
+                            <input required className=" w-full h-8 p-2 input-border" type="text" name="clienteCity" id="clienteCity" value={clienteSelect.field4}/>
                         </div>
                         <div className="w-1/6 ">
                             <label htmlFor="clienteEmail">Email</label>
-                            <input required className=" w-full h-8 p-2 input-border" type="email" name="clienteEmail" id="clienteEmail" value={clienteSelect.email}/>
+                            <input required className=" w-full h-8 p-2 input-border" type="email" name="clienteEmail" id="clienteEmail" value={clienteSelect.field3}/>
                         </div> 
                     <div className="w-1/6">
                     <label htmlFor="saleStatus">Estado de venta</label>
@@ -306,6 +182,147 @@ const NuevaVenta=()=>{
     );
 }
 
+const TablaProductos = ({ productos, setProductos, setProductosTabla,capturaNumber,setCapturaNumber }) => {
+
+  const [productoAAgregar, setProductoAAgregar] = useState({});
+  const [filasTabla, setFilasTabla] = useState([]);
+  const [buttonActive, setButtonActive] = useState('hidden');
+  
+
+
+  useEffect(() => {
+    if(Object.keys(productoAAgregar).length !== 0){
+      console.log(productoAAgregar);
+      setButtonActive("")  
+    }
+   
+  }, [productoAAgregar]);
+
+  useEffect(() => {
+    console.log('filasTabla', filasTabla);
+    setProductosTabla(filasTabla);
+  }, [filasTabla, setProductosTabla]);
+
+
+  useEffect(() => {
+    console.log(capturaNumber);
+    
+  },);
+
+
+  const agregarNuevoProducto = () => {
+    setFilasTabla([...filasTabla, productoAAgregar]);
+    setProductos(productos.filter((v) => v._id !== productoAAgregar._id));
+    setProductoAAgregar({});
+    setButtonActive("hidden")  
+  };
+
+  const eliminarProducto = (prodcutoAEliminar) => {
+    setFilasTabla(filasTabla.filter((v) => v._id !== prodcutoAEliminar._id));
+    setProductos([...productos, prodcutoAEliminar]);
+  };
+
+
+  const modificarProducto = (producto, cantidad) => {
+    const productoModificado = filasTabla.filter((v) => v._id === producto._id)[0];
+    productoModificado.cantidad = cantidad;
+    let ft = [...filasTabla];
+    ft = ft.filter((v) => v._id !== producto._id);
+    ft = [...ft, productoModificado];
+    setFilasTabla(ft);
+  };
+
+  return (
+    <>
+      <div className='flex w-full justify-between'>
+        <label className='flex flex-col' htmlFor='produto'>
+          <select
+            className='p-2'
+            value={productoAAgregar._id ?? ''}
+            onChange={(e) => setProductoAAgregar(productos.filter((v) => v._id === e.target.value)[0])}>
+            <option name="Seleccione un Producto" disabled value=''>
+              Seleccione un Producto
+            </option>
+            {productos.map((el) => {
+              return (
+                <option
+                  key={nanoid()}
+                  value={el._id}
+                >{`${el.field1} ${el.field6} ${el.field4} ${el.field7}`}</option>
+              );
+            })}
+          </select>
+        </label>
+        <button
+          type='button'
+          onClick={() => agregarNuevoProducto()}
+          className= {`col-span-2 bg-green-400 p-2 rounded-full shadow-md hover:bg-green-600 text-white ${buttonActive}`}>
+          Agregar Producto
+        </button>
+      </div>
+      <table className='table-fixed w-full bg-white border-solid border-gray-300 border my-5'>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Genero</th>
+            <th>Talla</th>
+            <th>Color</th>
+            <th>Cantidad</th>
+            <th>Precio Unitario</th>
+            <th>Valor total</th>
+            <th>Eliminar</th>
+            <th className='hidden'>Input</th>
+          </tr>
+        </thead>
+        <tbody>
+          {filasTabla.map((el, index) => {
+            return (
+
+
+              <FilaProducto
+              key={nanoid()}
+              producto={el}
+              index={index}
+              eliminarProducto={eliminarProducto}
+              modificarProducto={modificarProducto}
+            />
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+const FilaProducto =({producto , index,eliminarProducto,modificarProducto})=>{
+  return (
+
+    <tr key={nanoid()}>
+      <td align="center" >{producto.ids}</td>
+      <td align="center">{producto.field1}</td>
+      <td align="center">{producto.field6}</td>
+      <td align="center">{producto.field4}</td>
+      <td align="center">{producto.field7}</td>
+      <td align="center">
+        <label htmlFor={`cantidad_${index}`}>
+          <input value={producto.cantidad}  type='text' name={`cantidad_${index}`}  id={`cantidad_${index}` } 
+          onBlur={(e) => modificarProducto(producto, e.target.value)}/>
+        </label>  
+      </td>
+      <td align="center">$ {producto.field2}</td>
+      <td align="center">$ {producto.field2 *producto.cantidad}</td>
+      <td align="center">
+        <i
+          onClick={() => eliminarProducto(producto)}
+          className='fas fa-minus text-red-500 cursor-pointer'
+        />
+      </td>
+      <input hidden defaultValue={producto._id} name={`producto_${index}`} />
+  </tr>
+
+  );
+}
 
 
 
