4370ae101d90da873e37dd19013a2412de4f20b2
lilianaromerop <lilianaromerop11@gmail.com>
2021-10-15 16:26:30 -0500

    boton login

--- src/modules/login/Login.js
+++ src/modules/login/Login.js
@@ -15,8 +15,8 @@ const Login=()=>{
         <div className="logo text-7xl">SPORTY </div>
         <div className="text-2xl mb-9">SALES MANAGER </div>
         
-        <button className= "w-1/6 cursor-pointer bg-black h-10 text-green-100 font-semibold border border-green-100 rounded flex items-center justify-center"
-        onClick={() => loginWithRedirect()}>Ingresar</button>
+        <button className= "w-1/6 cursor-pointer bg-black h-10 text-gray-300 font-semibold border border-gray-300 rounded flex items-center justify-center"
+        onClick={() => loginWithRedirect()}>LOGIN</button>
         {/*<button onClick={() => logout({ returnTo: window.location.origin })}>
             Cerrar Sesión
     </button>*/}
