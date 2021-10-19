d2f1ec607f723bf1019a2799e86a9f0401bfbc80
lilianaromerop <lilianaromerop11@gmail.com>
2021-10-19 11:53:40 -0500

    ultimo cambio saviso sin permiso

--- src/modules/PrivateRoute.jsx
+++ src/modules/PrivateRoute.jsx
@@ -12,7 +12,7 @@ return children;
 return(
 
     <>
-        <div className="text-center mt-52 p-10">
+        <div className="text-center mt-40 p-10">
             <i class="fas fa-exclamation-triangle fa-4x"></i>
             <h1 className="text-2xl mt-6 font-bold">No tienes permiso para ver esta página</h1>
             <p className="mt-4 text-sm leading-4">Contacta al administrador para resolver<br/>
